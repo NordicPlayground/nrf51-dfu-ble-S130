4695b95aa7b497dc696a3108c2401f59959c22cb
hubuhubu <buihung@gmail.com>
2014-12-02 15:24:34 +0100

    Inittial commit

---
+++ arm/_build/bootloader_settings_arm.d
@@ -0,0 +1,13 @@
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Source\bootloader_dfu\bootloader_settings_arm.c
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\bootloader_dfu_Debug\bootloader_settings.h
+.\_build\bootloader_settings_arm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\bootloader_dfu_Debug\bootloader_types.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\bootloader_dfu_Debug\dfu_types.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\nrf51.h
+.\_build\bootloader_settings_arm.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm0.h
+.\_build\bootloader_settings_arm.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\_build\bootloader_settings_arm.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\system_nrf51.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\app_common\app_util.h
+.\_build\bootloader_settings_arm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\_build\bootloader_settings_arm.o: ..\..\..\..\..\Include\compiler_abstraction.h
